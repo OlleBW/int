c5298e1776ffeff1181b03c07963f497f6532541
Olle Brink Wennberg <olle.brinkwennberg@elev.ga.ntig.se>
2021-09-22 11:05:43 +0200

    jnlsmsdmsml

--- int/Program.cs
+++ int/Program.cs
@@ -19,7 +19,7 @@ if (Answer1 == "3")
     Console.ReadLine();
     Console.WriteLine("Of course you are!");
     Console.WriteLine("What is 16 + 20?");
-    
+
     
     string Answer2 = Console.ReadLine();
     if (Answer2 == "36")
@@ -29,18 +29,32 @@ if (Answer1 == "3")
         Console.WriteLine("Now for the last question");
         Console.WriteLine("Are you ready?");
         Console.ReadLine();
-        Console.WriteLine("");
+        Console.WriteLine("9817*783");
+        string Answer3 = Console.ReadLine();
+
+        while (Answer3 != "7686711")
+        {
+            Console.WriteLine("Try again.");
+            Console.ReadLine();
+        }
+        if (Answer3 == "7686711")
+        {
+            Console.WriteLine("Congratulations! You got the right answer and was awarded with 7686711 points");
+            Points = Points + 7686711;
+            Console.WriteLine("You passed the math quiz with a total of " + Points + " points");
+        }
+         
     }
     else
     {
         Console.WriteLine("That was wrong! Shame on you!");
+        Console.WriteLine("You failed the quiz with a total of 10 points.");
     }
 }
 else
 {
     Console.WriteLine("That was very incorrect. You failed!");
     Console.WriteLine("You got a total of 0 points");
-
 }
 
 Console.ReadLine();
